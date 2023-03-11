d4415bba8aad5f47e360c87c5fbdc5edd3d7c35c
Omprakash Thakur <iewd1285@gmail.com>
2023-03-11 12:50:21 +0530

    plugin header

---
+++ grocery-shop.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Grocery Shop
+Plugin URI: https://github.com/iemd/wp-example-plugin
+Description: Create a Grocery Store to display product information
+Version: 1.0
+Author: Omprakash Thakur
+Author URI: https://github.com/iemd
+License: GPLv2
+*/
