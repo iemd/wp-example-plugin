5b22ab9a0c282628bf167c2bbdf95f3200c75210
Omprakash Thakur <iewd1285@gmail.com>
2023-03-11 12:59:08 +0530

    registering the custom post type for products

--- grocery-shop.php
+++ grocery-shop.php
@@ -42,3 +42,46 @@ function grocery_shop_install() {
     //save our default option values
     update_option( 'grocery_options', $gcery_options_arr );
 }
+
+/**
+ * REGISTERING THE CUSTOM POST TYPE FOR PRODUCTS
+ */
+// Action hook to initialize the plugin
+add_action( 'init', 'grocery_shop_init' );
+
+// Initialize the Grocery Shop
+function grocery_shop_init() {
+
+    //register the products custom post type
+    $labels = array(
+        'name'                  => __( 'Products', 'grocery-plugin' ),
+        'singular_name'         => __( 'Product', 'grocery-plugin' ),
+        'add_new'               => __( 'Add New', 'grocery-plugin' ),
+        'add_new_item'          => __( 'Add New Product', 'grocery-plugin' ),
+        'edit_item'             => __( 'Edit Product', 'grocery-plugin' ),
+        'new_item'              => __( 'New Product', 'grocery-plugin' ),
+        'all_items'             => __( 'All Products', 'grocery-plugin' ),
+        'view_item'             => __( 'View Product', 'grocery-plugin' ),
+        'search_items'          => __( 'Search Products', 'grocery-plugin' ),
+        'not_found'             => __( 'No products found', 'grocery-plugin' ),
+        'not_found_in_trash'    => __( 'No products found in Trash', 'grocery-plugin' ),
+        'menu_name'             => __( 'Products', 'grocery-plugin' )
+    );
+
+    $args = array(
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'query_var'             => true,
+        'rewrite'               => true,
+        'capability_type'       => 'post',
+        'has_archive'           => true,
+        'hierarchical'          => false,
+        'menu_position'         => null,
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'excerpt' )
+    );
+
+    register_post_type( 'grocery-products', $args );
+}
